68d5467b4e08b5a7784f12789330283998fa9654
piletskiy <piletskiy10@gmail.com>
2022-02-07 21:17:59 +0200

    ADD = update version and export changed

--- index.ts
+++ index.ts
@@ -1,2 +1,8 @@
-export {Field} from './src/Field';
-export {FieldsCheckOutput, ValidateError} from './src/FieldsCheckOutput';
+import {Field} from './src/Field';
+import {FieldsCheckOutput, ValidateError} from './src/FieldsCheckOutput';
+
+export {
+    Field,
+    FieldsCheckOutput,
+    ValidateError
+}
