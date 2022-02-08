9f89cf129b71423668356822eeb23c5ba55bb4fa
piletskiy <piletskiy10@gmail.com>
2022-02-08 09:50:50 +0200

    utils time fix

--- index.ts
+++ index.ts
@@ -1,2 +1,6 @@
-export {Field} from './src/Field';
-export {FieldsCheckOutput, ValidateError} from './src/FieldsCheckOutput';
+import {Utils} from "./src/Utils";
+
+console.log(Utils.formatDate('2020-02-01', "YYYY-MM-DD HH:mm:ss"))
+
+// export {Field} from './src/Field';
+// export {FieldsCheckOutput, ValidateError} from './src/FieldsCheckOutput';
--- src/Utils.ts
+++ src/Utils.ts
@@ -2,20 +2,20 @@ export class Utils {
     public static formatDate(date: string, format: "YYYY-MM-DD" | "YYYY-MM-DD HH:mm:ss") {
         let d = new Date(date);
 
-        let month = '' + (d.getMonth() + 1),
-            day = '' + d.getDate(),
-            year = d.getFullYear();
+        let month = '' + (d.getUTCMonth() + 1),
+            day = '' + d.getUTCDate(),
+            year = d.getUTCFullYear();
 
-        let hours = '' + d.getHours(),
-            minutes = '' + d.getMinutes(),
-            seconds = '' + d.getSeconds();
+        let hours = '' + d.getUTCHours(),
+            minutes = '' + d.getUTCMinutes(),
+            seconds = '' + d.getUTCSeconds();
 
-        if (month.length > 2) month = '0' + month;
-        if (day.length > 2) day = '0' + day;
+        if (month.length < 2) month = '0' + month;
+        if (day.length < 2) day = '0' + day;
 
-        if (hours.length > 2) hours = '0' + hours;
-        if (minutes.length > 2) minutes = '0' + minutes;
-        if (seconds.length > 2) seconds = '0' + seconds;
+        if (hours.length < 2) hours = '0' + hours;
+        if (minutes.length < 2) minutes = '0' + minutes;
+        if (seconds.length < 2) seconds = '0' + seconds;
 
         switch (format) {
             case "YYYY-MM-DD":
