7baae051a3e64cb5a6ebd0768b51533c874f81d3
piletskiy <piletskiy10@gmail.com>
2022-02-08 09:52:45 +0200

    version up to 1.0.1 and export fix

--- index.ts
+++ index.ts
@@ -1,6 +1,2 @@
-import {Utils} from "./src/Utils";
-
-console.log(Utils.formatDate('2020-02-01', "YYYY-MM-DD HH:mm:ss"))
-
-// export {Field} from './src/Field';
-// export {FieldsCheckOutput, ValidateError} from './src/FieldsCheckOutput';
+export {Field} from './src/Field';
+export {FieldsCheckOutput, ValidateError} from './src/FieldsCheckOutput';
